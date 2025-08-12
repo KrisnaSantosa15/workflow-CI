e8e61600883693361d0a2b80434f9de7d28471b6
krisnasantosa15 <mamang.krisna15@gmail.com>
2025-08-12 23:42:30 +0700

    fix error

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Correct URL for the running MLflow model server
-URL = "http://localhost:5000/invocations"
+URL = "http://localhost:5001/invocations"
 
 # Load input data
 with open("model_dir/model/serving_input_example.json") as f:
